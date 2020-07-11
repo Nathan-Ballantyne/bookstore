8b02d78b460e59c3a5ecfdfc0a68115d1ec67965
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2020-07-11 15:46:53 +1200

    add bycrypt package

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"strconv"
 
+	//"github.com/Nathan-Ballantyne/bookstore/pkg/forms"
 	"github.com/Nathan-Ballantyne/bookstore/pkg/models"
 )
 
@@ -20,9 +21,34 @@ func (app *application) allBooks(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("All books in the system"))
 }
 
-func (app *application) addBook(w http.ResponseWriter, r *http.Request) {
+func (app *application) addBook(w http.ResponseWriter, r *http.Request) int {
 	app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
-	w.Write([]byte("All books in the system"))
+
+	// Parse the form data.
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return 0
+	}
+	// Validate the form contents using the form helper we made earlier.
+	form := r.PostForm
+	// Try to create a new user record in the database. If the email already exists
+	// add an error message to the form and re-display it.
+	relYear, err := strconv.Atoi(form.Get("release_year"))
+	pageCount, err := strconv.Atoi(form.Get("page_count"))
+	rating, err := strconv.Atoi(form.Get("rating"))
+	id, err := app.books.Insert(form.Get("title"), form.Get("author"), form.Get("cover"),
+		form.Get("series"), form.Get("read_status"), relYear, pageCount, rating)
+
+	if err != nil {
+		if errors.Is(err, models.ErrDuplicateEmail) {
+			fmt.Fprint(w, "An error occured")
+		} else {
+			app.serverError(w, err)
+		}
+		return 0
+	}
+	return id
 }
 
 func (app *application) getBook(w http.ResponseWriter, r *http.Request) {
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/bmizerany/pat v0.0.0-20170815010413-6226ea591a40
 	github.com/go-sql-driver/mysql v1.5.0
+	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899
 )
