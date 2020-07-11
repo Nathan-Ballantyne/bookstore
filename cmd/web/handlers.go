ce2a087f37b72a85cf076259e2a2b1c1d6dd2560
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2020-07-12 10:31:07 +1200

    add sign up user and login user handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 	"strconv"
 
-	//"github.com/Nathan-Ballantyne/bookstore/pkg/forms"
+	"github.com/Nathan-Ballantyne/bookstore/pkg/forms"
 	"github.com/Nathan-Ballantyne/bookstore/pkg/models"
 )
 
@@ -77,4 +77,75 @@ func (app *application) getBook(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprint(w, string(bookJSON))
+}
+
+func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	// Check whether the credentials are valid. If they're not, add a generic error
+	// message to the form failures map and re-display the login page.
+	form := forms.New(r.PostForm)
+	_, err = app.users.Authenticate(form.Get("email"), form.Get("password"))
+	if err != nil {
+		if errors.Is(err, models.ErrInvalidCredentials) {
+			form.Errors.Add("generic", "Email or Password is incorrect")
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	// Add the ID of the current user to the session, so that they are now 'logged
+	// in'.
+	//app.session.Put(r, "authenticatedUserID", id)
+
+	// Redirect the user to the home page.
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+
+}
+
+func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
+	// Parse the form data.
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	// Validate the form contents using the form helper we made earlier.
+	form := forms.New(r.PostForm)
+	form.Required("name", "email", "password")
+	form.MaxLength("name", 255)
+	form.MaxLength("email", 255)
+	form.MatchesPattern("email", forms.EmailRX)
+	form.MinLength("password", 10)
+
+	// If there are any errors, redisplay the signup form.
+	if !form.Valid() {
+		// return some json string
+		return
+	}
+
+	// Try to create a new user record in the database. If the email already exists
+	// add an error message to the form and re-display it.
+	err = app.users.Insert(form.Get("name"), form.Get("email"), form.Get("password"))
+	if err != nil {
+		if errors.Is(err, models.ErrDuplicateEmail) {
+			form.Errors.Add("email", "Address is already in use")
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	// Otherwise add a confirmation flash message to the session confirming that
+	// their signup worked and asking them to log in.
+	//app.session.Put(r, "flash", "Your signup was successful. Please log in.")
+
+	// And redirect the user to the login page.
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
